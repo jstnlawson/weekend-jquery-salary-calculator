13120c2ae3ebf7871f28c5266babba40d67097f1
Justin Lawson <jstn.lawson@gmail.com>
2023-06-10 12:40:00 -0500

    submit button enters data and clears

--- script.js
+++ script.js
@@ -2,13 +2,15 @@
 $(document).ready(onReady);
 
 
-//hit submit and add new row of data to the form
+//✅ hit submit and add new row of data to the form/ clear data from inputs
 //calculate monthly cost and add it to total
 //if it exceeds 20,000 add a red backgroung to the monthly cost
 //add delete button that removes employee(don't need to remove cost)
 
 function onReady() {
     console.log('onReady is go!')
+
+    $('.submit-button').on('click', submitData)
 
 
 
@@ -26,16 +28,26 @@ function submitData(event) {
     let titleVal = $('#title-input').val()
     let salaryVal = $('#salary-input').val()
 
-    // let formattedEmployeeData = `${firstNameVal} (${lastNameVal} ${idVal} ${titleVal} ${salaryVal})` 
+    //let formattedEmployeeData = `${firstNameVal} (${lastNameVal} ${idVal} ${titleVal} ${salaryVal})` 
+    //I'm not sure how to do the syntax here so I did it the long way below
 
     $('.employee-data').append(`
-        <td>${firstNameVal}</td>
-        <td>${lastNameVal}</td>
-        <td>${idVal}</td>
-        <td>${titleVal}</td>
-        <td>${salaryVal}</td>
-        <td><button class="delete">delete</button></td>
+        <tr>
+            <td>${firstNameVal}</td>
+            <td>${lastNameVal}</td>
+            <td>${idVal}</td>
+            <td>${titleVal}</td>
+            <td>${salaryVal}</td>
+            <td><button class="delete">delete</button></td>
+        </tr>
     `)
+
+    $('#first-input').val('')
+    $('#last-input').val('')
+    $('#id-input').val('')
+    $('#title-input').val('')
+    $('#salary-input').val('')
 
 
 }
+       
